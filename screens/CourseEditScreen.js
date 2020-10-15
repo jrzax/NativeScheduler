ed02cad95dd3a49a06564b61757328b0a51d4b25
jrzax <jrzax@aol.com>
2020-10-14 22:20:00 -0500

    Firebase store

--- screens/CourseEditScreen.js
+++ screens/CourseEditScreen.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as Yup from 'yup';
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Form from '../components/Form';
+import {firebase} from '../firebase'
 
 const Field = ({label, value}) => {
   return (
@@ -12,19 +13,30 @@ const Field = ({label, value}) => {
   );
 };
 
-const CourseEditScreen = ({navigation, route}) => {
+const CourseEditScreen = ({ navigation, route }) => {
   const course = route.params.course;
+  const [submitError, setSubmitError] = useState('');
+
+  async function handleSubmit(values) {
+    const { id, meets, title } = values;
+    const course = { id, meets, title };
+    firebase.database().ref('courses').child(id).set(course).catch(error => {
+      setSubmitError(error.message);
+    });
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-      <Form
-      validationSchema={validationSchema}
-      initialValues={{
-        id: course.id,
-        meets: course.meets,
-        title: course.title,
-      }}>
+        <Form
+          initialValues={{
+            id: course.id,
+            meets: course.meets,
+            title: course.title,
+          }}
+          validationSchema={validationSchema}
+          onSubmit={values => handleSubmit(values)}
+        >
           <Form.Field
             name="id"
             leftIcon="identifier"
@@ -43,11 +55,13 @@ const CourseEditScreen = ({navigation, route}) => {
             leftIcon="format-title"
             placeholder="Introduction to programming"
           />
+          <Form.Button title={'Update'} />
+          {<Form.ErrorMessage error={submitError} visible={true} />}
         </Form>
       </ScrollView>
     </SafeAreaView>
   );
-};
+  };
 
 const validationSchema = Yup.object().shape({
     id: Yup.string()
